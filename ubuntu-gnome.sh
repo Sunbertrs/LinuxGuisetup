3931ea4436e218cb0bf225f4d30090294eb623f8
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-16 15:01:04 +0800

    Delete ubuntu-gnome.sh

--- ubuntu-gnome.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash 
-echo "[+] Installing GNOME" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install gnome-shell xorg xrdp 
-echo "[+] Configuring XRDP to listen on port 3390 ..." && sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini && echo "[+] Installed." 
