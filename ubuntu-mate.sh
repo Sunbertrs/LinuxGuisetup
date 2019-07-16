33d77202694b032946851161865e7ef962f0601d
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-16 15:33:56 +0800

    Add files via upload

---
+++ ubuntu-mate.sh
@@ -0,0 +1,2 @@
+#! /bin/bash 
+echo "[+] Installing XFCE4" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install ubuntu-mate-core ubuntu-mate-desktop xorg && echo "[+] Installed." 
