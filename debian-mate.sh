2e88092283a09fc943c96ce017f1d73946013005
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-30 14:07:51 +0800

    Update debian-mate.sh

--- debian-mate.sh
+++ debian-mate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "[+] Installing MATE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install --yes --force-yes mate-desktop-environment xorg xrdp 
-echo "[+] Configuring XRDP to listen on port 3390 ..." && sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini && echo "[+] Installed." 
+echo "[+] Installing MATE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install --yes --force-yes mate-desktop-environment xorg vnc4server tightvncserver 
+echo "[+] Installed." 
