22039dd53fdcdaf6751fe609b79b66cb48bf4b72
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-30 14:26:00 +0800

    Update ubuntu-xfce4.sh

--- ubuntu-xfce4.sh
+++ ubuntu-xfce4.sh
@@ -1,3 +1,3 @@
 #! /bin/bash 
-echo "[+] Installing XFCE4" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install xfce4 xorg xrdp 
-echo "[+] Configuring XRDP to listen on port 3390 ..." && sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini && echo "[+] Installed." 
+echo "[+] Installing XFCE4" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install xfce4 xorg vnc4server tightvncserver 
+echo "[+] Installed." 
