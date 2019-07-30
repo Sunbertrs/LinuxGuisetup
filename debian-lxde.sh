f97443afe71799cf879db718963fd9a8a2e63e07
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-30 14:02:24 +0800

    Update debian-lxde.sh

--- debian-lxde.sh
+++ debian-lxde.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "[+] Installing LXDE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install lxde-core xorg xrdp 
-echo "[+] Configuring XRDP to listen on port 3390 ..." && sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini && echo "[+] Installed." 
+echo "[+] Installing LXDE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install lxde-core xorg vnc4server tightvncserver 
+echo "[+] Installed." 
