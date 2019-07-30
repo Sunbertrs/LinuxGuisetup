27755f1bf4f1a34aa8c23e8d017c52d66ff70cd7
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-07-30 14:21:12 +0800

    Update kali-xfce4.sh

--- kali-xfce4.sh
+++ kali-xfce4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "[+] Installing XFCE4" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install kali-desktop-xfce xorg xrdp 
-echo "[+] Configuring XRDP to listen on port 3390 ..." && sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini && echo "[+] Installed." 
+echo "[+] Installing XFCE4" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install kali-desktop-xfce xorg vnc4server tightvncserver 
+echo "[+] Installed." 
