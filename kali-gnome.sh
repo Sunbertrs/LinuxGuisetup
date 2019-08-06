d661befa9ef8bbec62078bbd001c230349acbf0f
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-08-06 16:09:25 +0800

    Update kali-gnome.sh

--- kali-gnome.sh
+++ kali-gnome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "[+] Installing GNOME" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install --yes --force-yes apt-get install gnome-core desktop-base xorg xrdp 
-echo "[+] Configuring XRDP to listen on port 3390 ..." && sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini && echo "[+] Installed." 
+echo "[+] Installing GNOME" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install --yes --force-yes apt-get install gnome-core desktop-base xorg tightvncserver vnc4server 
+echo "[+] Installed." 
