12c34551a0426f74b636b9fd5dc223152439b5f7
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-08-06 16:36:53 +0800

    Update kali-lxde.sh

--- kali-lxde.sh
+++ kali-lxde.sh
@@ -1,3 +1,3 @@
 #! /bin/bash 
-echo "[+] Installing LXDE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install lxde-core lxde xorg vnc4server tightvncserver 
+echo "[+] Installing LXDE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install lxde-core lxde xorg vnc4server tightvncserver 
 echo "[+] Installed." 
