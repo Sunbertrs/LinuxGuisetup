504af68fdab71e10bc2588fa4f70a4ac98c4c1ee
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-08-06 16:36:12 +0800

    Update kali-gnome.sh

--- kali-gnome.sh
+++ kali-gnome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "[+] Installing GNOME" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install --yes --force-yes apt-get install gnome-core desktop-base xorg tightvncserver vnc4server 
+echo "[+] Installing GNOME" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install apt-get install gnome-core desktop-base xorg tightvncserver vnc4server 
 echo "[+] Installed." 
