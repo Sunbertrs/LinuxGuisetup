7ba21f37b2c4808d2751084fc826e9699b9c6575
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2019-08-06 16:35:39 +0800

    Update debian-mate.sh

--- debian-mate.sh
+++ debian-mate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "[+] Installing MATE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get install --yes --force-yes mate-desktop-environment xorg vnc4server tightvncserver 
+echo "[+] Installing MATE" && apt-get update && apt-get dist-upgrade -y --force-yes && apt-get --yes --force-yes install mate-desktop-environment xorg vnc4server tightvncserver 
 echo "[+] Installed." 
